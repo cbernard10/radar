e8d743722de57cab6ce6968ee992dc61272830a2
cbernard10 <clement.bernard@protonmail.com>
2023-04-25 14:32:14 +0200

    mise à jour des fichiers pour installation
    installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='radar',
+    version='0.1.0',
+    description='radar package',
+    url='https://github.com/cbernard10/radar',
+    author='Clément Bernard',
+    author_email='clement.bernard@protonmail.com',
+    license='MIT',
+    packages=['radarpkg2'],
+    install_requires=['numpy']
+)
